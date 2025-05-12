c0c2e3f8677a0da8f1bec46bac81dfd6a7206a6a
Dean Marano <dean.marano@hashicorp.com>
2025-05-12 11:23:58 -0400

    Update recommended.

--- recommended.mjs
+++ recommended.mjs
@@ -1,27 +1,29 @@
 import * as emberA11yTesting from "./lib/index.js";
 
-export default {
-  languageOptions: {
-    ecmaVersion: 2018,
-    sourceType: "module",
-    globals: {
-      browser: true,
-    },
-  },
-  plugins: {
-    "ember-a11y-testing": emberA11yTesting,
-  },
-  settings: {
-    "ember-a11y-testing": {
-      auditModule: {
-        package: "ember-a11y-testing/test-support/audit",
-        exportName: "default",
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: 2018,
+      sourceType: "module",
+      globals: {
+        browser: true,
       },
     },
+    plugins: {
+      "ember-a11y-testing": emberA11yTesting,
+    },
+    settings: {
+      "ember-a11y-testing": {
+        auditModule: {
+          package: "ember-a11y-testing/test-support/audit",
+          exportName: "default",
+        },
+      },
+    },
+    rules: {
+      "ember-a11y-testing/a11y-audit-after-test-helper": "error",
+      "ember-a11y-testing/a11y-audit-no-expression": "error",
+      "ember-a11y-testing/a11y-audit-no-globals": "error",
+    },
   },
-  rules: {
-    "ember-a11y-testing/a11y-audit-after-test-helper": "error",
-    "ember-a11y-testing/a11y-audit-no-expression": "error",
-    "ember-a11y-testing/a11y-audit-no-globals": "error",
-  },
-};
+];
