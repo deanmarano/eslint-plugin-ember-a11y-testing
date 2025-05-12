3d8f466ba527b7574766b511077c327436bf2367
Dean Marano <dean.marano@hashicorp.com>
2025-05-12 11:34:11 -0400

    WIP

--- recommended.mjs
+++ recommended.mjs
@@ -1,31 +1,3 @@
 import emberA11yTesting from "./lib/index.js";
 
-const { rules } = emberA11yTesting;
-
-export default [
-  {
-    languageOptions: {
-      ecmaVersion: 2018,
-      sourceType: "module",
-      globals: {
-        browser: true,
-      },
-    },
-    plugins: {
-      "ember-a11y-testing": emberA11yTesting,
-    },
-    settings: {
-      "ember-a11y-testing": {
-        auditModule: {
-          package: "ember-a11y-testing/test-support/audit",
-          exportName: "default",
-        },
-      },
-    },
-    rules: {
-      "ember-a11y-testing/a11y-audit-after-test-helper": rules["a11y-audit-after-test-helper"],
-      "ember-a11y-testing/a11y-audit-no-expression": rules["a11y-audit-no-expression"],
-      "ember-a11y-testing/a11y-audit-no-globals": rules["a11y-audit-no-globals"],
-    },
-  },
-];
+export default emberA11yTesting.configs.recommended;
